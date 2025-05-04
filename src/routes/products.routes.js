fa478162ce5811d5a0e5f92e03f64eb8825c0aa4
sebareibold <sebastianreibold2003@gmail.com>
2025-05-04 16:40:41 -0300

    Implementacion de carts.routes y products.routes

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -5,7 +5,9 @@ const productManager = require("../managers/ProductsManager");
 // Rutas relacionadas a Carritos
 router.get("/", async (req, res) => {
   try {
-    res.status(200).json({ success: true, message: "GET todos los productos" });
+    const products = productManager.getProducts();
+
+    res.status(200).json({ success: true, products });
   } catch (e) {
     res.status(500).json({ success: false, error: e.message });
   }
@@ -13,23 +15,99 @@ router.get("/", async (req, res) => {
 
 router.get("/:pid", async (req, res) => {
   try {
-    res.status(200).json({ success: true, message: "GET todos los productos" });
+    const pid = req.params.pid;
+    const productId = parseInt(pid);
+
+    if (isNaN(productId)) {
+      return res.status(400).json({
+        success: false,
+        error: "ID de prodcuto inválido. Debe ser un número.",
+      });
+    }
+    const prod = await productManager.getProductById(productId);
+    res.status(200).json({ success: true, prod });
   } catch (e) {
-    res.status(500).json({ success: false, error: e.message });
+    //Vemos si el error es de route o managers
+    if (e.message && e.message.includes("no encontrado")) {
+      res.status(404).json({
+        success: false,
+        error: e.message,
+      });
+    } else {
+      res.status(500).json({ success: false, error: e.message });
+    }
   }
 });
 
 router.post("/:pid", async (req, res) => {
+  const { id, name, price, stock, category, description } = req.body;
+
+  if (!id || !name || !price || !stock || !category || !description) {
+    return res.status(400).json({
+      success: false,
+      error: "El cuerpo de la petición debe contener att correctos",
+    });
+  }
+  const newProd = await productManager.addProduct(
+    id,
+    name,
+    price,
+    stock,
+    category,
+    description
+  );
   try {
-    res.status(200).json({ success: true, message: "GET todos los productos" });
+    res.status(201).json({ success: true, producto: newProd });
   } catch (e) {
-    res.status(500).json({ success: false, error: e.message });
+    if (e.message && e.message.includes("no encontrado")) {
+      res.status(404).json({
+        success: false,
+        error: e.message,
+      });
+    } else {
+      res.status(500).json({ success: false, error: e.message });
+    }
   }
 });
 
 router.put("/:pid", async (req, res) => {
   try {
-    res.status(200).json({ success: true, message: "GET todos los productos" });
+    const pid = req.params.pid;
+    const productId = parseInt(pid);
+
+    if (isNaN(productId)) {
+      return res.status(400).json({
+        success: false,
+        error: "ID de prodcuto inválido. Debe ser un número.",
+      });
+    }
+
+    const { valor, campo } = req.body;
+
+    if (
+      !valor ||
+      campo !== "name" ||
+      campo !== "price" ||
+      campo !== "stock" ||
+      campo !== "category" ||
+      campo !== "description"
+    ) {
+      return res.status(400).json({
+        success: false,
+        error:
+          "El cuerpo de la petición debe contener el body correcto (formato)",
+      });
+    }
+
+    const exito = await productManager.updateProduct(productId, campo, valor);
+    if (exito) {
+      res.status(200).json({ success: true, message: "Actualizacion Exitosa" });
+    } else {
+      return res.status(400).json({
+        success: false,
+        error: "Error en la actualizacion, verificar body.)",
+      });
+    }
   } catch (e) {
     res.status(500).json({ success: false, error: e.message });
   }
@@ -37,7 +115,28 @@ router.put("/:pid", async (req, res) => {
 
 router.delete("/:pid", async (req, res) => {
   try {
-    res.status(200).json({ success: true, message: "GET todos los productos" });
+    const pid = req.params.pid;
+    const productId = parseInt(pid);
+
+    if (isNaN(productId)) {
+      return res.status(400).json({
+        success: false,
+        error: "ID de prodcuto inválido. Debe ser un número.",
+      });
+    }
+    const exito = await productManager.deleteProduct(productId);
+    if (exito) {
+      res
+        .status(200)
+        .json({ success: true, message: "Exito en la eliminacion" });
+    } else {
+      res
+        .status(200)
+        .json({
+          success: true,
+          error: "Error en la eliminacion, verificar body.)",
+        });
+    }
   } catch (e) {
     res.status(500).json({ success: false, error: e.message });
   }
--- src/server.js
+++ src/server.js
@@ -7,12 +7,25 @@ const cartsRouter = require("./routes/carts.routes");
 
 /*----------------------------- Middlewares ----------------------------- */
 app.use(express.json()); //Recibir datos por body {}
-app.use(express.urlencoded({extended: true})); //Data por From
+app.use(express.urlencoded({ extended: true })); //Data por From
 
 app.use("/api/products", productsRouter); // Uso de rutas de productos
 app.use("/api/carts", cartsRouter); // Uso de rutas de carritos
 
-module.exports = app
+/*----------------------------- Bienvenida ----------------------------- */
+app.get("/", async (req, res) => {
+  res.send(`
+        <h1>¡Bienvenido a mi primera APIRest!</h1>
+        <h4>Podes ver productos :) y Carritos :)</h4>
+
+        <p>Productos con: /api/products/  </p>
+        <p>Carritos con: /api/carts/  </p>
+
+        
+    `);
+});
+
+module.exports = app;
 
 /*
     En si su rol es actuar como un archivo configurador, donde se crea la aplicacion de Express,
