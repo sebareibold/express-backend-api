91de7d4c9d9b1060a5c5dc30d0baca10d16df058
sebareibold <sebastianreibold2003@gmail.com>
2025-05-22 17:46:12 -0300

    Update

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+//------------------------------ Módulos Requeridos ------------------------------
 const express = require("express");
 const app = express(); //App actua como servidor
 
@@ -5,10 +6,24 @@ const handlebars = require("express-handlebars");
 const path = require("path");
 const productManager = require("./managers/ProductsManager");
 
-/*--------------------------- Seteo Handlebar ----------------------------- */
+//----------------------------- Importación API  ------------------------------------------
+const productsRouter = require("./routes/products.routes");
+const cartsRouter = require("./routes/carts.routes");
+
+//-------------------------------- Middlewares Globales --------------------------------------------
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//----------------------------- Enrutamiento API y Vistas ------------------------------------------
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
+
+//-------------------------- Configuración de Handlebars ------------------------------------
 app.engine(
   "hbs",
   handlebars.engine({
+    layoutsDir: path.join(__dirname, "views/layouts"),
+    partialsDir: path.join(__dirname, "views/partials"), // ¡Esta es la clave!
     extname: ".hbs",
     defaultLayout: "main",
   })
@@ -19,30 +34,26 @@ app.set("views engine", "handlebars");
 app.use(express.static(path.join(__dirname, "/views")));
 app.use(express.static(path.join(__dirname, "public")));
 
-/*--------------------------- Importamos Rutas ----------------------------- */
-const productsRouter = require("./routes/products.routes");
-const cartsRouter = require("./routes/carts.routes");
-
-/*----------------------------- Middlewares ----------------------------- */
-app.use(express.json()); //Recibir datos por body {}
-app.use(express.urlencoded({ extended: true })); //Data por From
-
-app.use("/api/products", productsRouter); // Uso de rutas de productos
-app.use("/api/carts", cartsRouter); // Uso de rutas de carritos
-
-/*----------------------------- Home con Handlebars ----------------------------- */
+//----------------------------- Ruta Principal "Home" con Handlebars -------------------------------
 app.get("/", async (req, res) => {
   const products = productManager.getProducts();
   //console.log(products);
   res.render("index.hbs", { products: products });
 });
 
+//----------------------------- Ruta "Real Time Products" con Handlebars -------------------------------
+app.get("/realtimeproducts", async (req, res) => {
+  const products = productManager.getProducts();
+  //console.log(products);
+  res.render("realTimeProducts.hbs", { products: products });
+});
+
 module.exports = app;
 
 /*
-    
-    En si su rol es actuar como un archivo configurador, donde se crea la aplicacion de Express,
-    se configura los MIddlewares globales, como los datos .sjon, las rutas, etc. Y se exporta la 
-    app para q la use el index
+
+En si su rol es actuar como un archivo configurador, donde se crea la aplicacion de Express,
+se configura los MIddlewares globales, como los datos .sjon, las rutas, etc. Y se exporta la 
+app para q la use el index
 
 */
